60c759db724d2803c766ba45f3b893ab193c9f32
고정완 (Can) <jw.gho@ecubelabs.com>
2020-04-05 00:03:05 +0900

    Fix typo

--- src/Iterator/Sample/Main.java
+++ src/Iterator/Sample/Main.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf(10);
         bookShelf.appendBook(new Book("Around the World in 80 Days"));
         bookShelf.appendBook(new Book("Bible"));
         bookShelf.appendBook(new Book("Cinderella"));
         bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+        bookShelf.appendBook(new Book("East of Eden"));
+        bookShelf.appendBook(new Book("Frankenstein"));
+        bookShelf.appendBook(new Book("Gulliver's Travels"));
+        bookShelf.appendBook(new Book("Hamlet"));
         Iterator it = bookShelf.iterator();
         while (it.hasNext()) {
-            Book book = (Book)it.next();
+            Book book = (Book) it.next();
             System.out.println(book.getName());
         }
     }
